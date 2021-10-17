2aada12dafae39ff4da594ff2ce66ae4a0da00ff
takurinton <takurinton@takurinton.com>
2021-10-18 06:43:48 +0900

    add: memo

--- chapter3/main.rs
+++ chapter3/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("{}", "hello world");
+    c3_2();
+}
+
+// 構造体
+// c3_1 にしてたら命名でエラー出た。
+// struct Chapter31Struct {
+//     // String は構造体
+//     name: String,
+//     age: i32, 
+// }
+
+// メソッド呼び出し
+// static method は :: で呼び出す
+// instance method は . で呼び出す
+fn c3_2() {
+    // String instance を生成、Java の String みたいな感覚かな
+    // シンタックスはPHP的な？
+    let s = String::from("hello world");
+    println!("{}, {}", s, s.len()); // こっちで読んでるのは instance method
 }
